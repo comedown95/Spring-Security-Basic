03c05eda6b824c4bb17bb375e93b4946dde89abf
comedown95 <qorwhdr@gmail.com>
2024-03-30 00:28:57 +0900

    간단한 회원가입 기능 및 회원 인증 기능 추가

--- src/main/java/com/security/basic/config/SecurityConfig.java
+++ src/main/java/com/security/basic/config/SecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,12 +14,26 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/", "/login").permitAll()
-                .requestMatchers("/h2-console/**").hasRole("ADMIN")
+                .requestMatchers("/", "/login", "/join").permitAll()
                 .requestMatchers("/profile/**").hasAnyRole("ADMIN", "USER")
                 .anyRequest().authenticated()
-        ).build();
+        ).formLogin(auth -> auth
+                .loginPage("/login")
+                .loginProcessingUrl("/login")
+                .permitAll()
+        ).csrf(AbstractHttpConfigurer::disable).build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/h2-console/**");
     }
 }
---
+++ src/main/java/com/security/basic/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.security.basic.repository;
+
+import com.security.basic.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    boolean existsByUsername(String username);
+
+    UserEntity findByUsername(String username);
+}
