a9ebf26c3cb63085260a86323d828eb4bb60ea47
comedown95 <qorwhdr@gmail.com>
2024-04-02 21:37:00 +0900

    간단한 시큐리티 설정 정보 추가

---
+++ src/main/java/com/security/basic/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.security.basic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                //csrf disable
+                .csrf((auth) -> auth.disable())
+                //From 로그인 방식 disable
+                .formLogin((auth) -> auth.disable())
+                //http basic 인증 방식 disable
+                .httpBasic((auth) -> auth.disable())
+                //경로별 인가 작업
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                //세션 설정
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
