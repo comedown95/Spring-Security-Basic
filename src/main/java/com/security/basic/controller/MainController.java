d18ac05f035da795c5921c4cdfc0bd9986d78644
comedown95 <qorwhdr@gmail.com>
2024-04-02 21:42:45 +0900

    MainController Rest로 변경

--- src/main/java/com/security/basic/controller/MainController.java
+++ src/main/java/com/security/basic/controller/MainController.java
@@ -1,11 +1,9 @@
 package com.security.basic.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+@RestController
 public class MainController {
 
     @GetMapping("/")
