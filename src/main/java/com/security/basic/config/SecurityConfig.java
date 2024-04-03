00a7ba1931bebe14dace8aa17d8bd853673b57c0
comedown95 <qorwhdr@gmail.com>
2024-04-03 23:08:21 +0900

    로그인 구현

--- src/main/java/com/security/basic/config/SecurityConfig.java
+++ src/main/java/com/security/basic/config/SecurityConfig.java
@@ -1,24 +1,42 @@
 package com.security.basic.config;
 
+import com.security.basic.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
+        this.customOAuth2UserService = customOAuth2UserService;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf((csrf) -> csrf.disable())
                 .formLogin((login) -> login.disable())
                 .httpBasic((basic) -> basic.disable())
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login((oauth2) -> oauth2
+                        // 커스텀 로그인 페이지 설정시 사용
+                        // .loginPage("/login")
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService)))
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
+                        .requestMatchers("/").permitAll()
                         .anyRequest().authenticated())
                 .build();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/h2-console/**");
     }
 }
---
+++ src/main/java/com/security/basic/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.security.basic.repository;
+
+import com.security.basic.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
